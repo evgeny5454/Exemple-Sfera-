6c8c6e8e36ac3eac1a50184fd91010844ee0c691
Dell <evgenyminaenkov@gmail.com>
2022-10-27 16:33:50 +0300

    refactor

--- app/src/main/java/com/evgeny5454/exemplesfera/MainActivity.kt
+++ app/src/main/java/com/evgeny5454/exemplesfera/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.evgeny5454.exemplesfera
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.evgeny5454.exemplesfera.adapters.AddPhotoAdapter
-import com.evgeny5454.exemplesfera.adapters.ChroniclesAdapter
-import com.evgeny5454.exemplesfera.adapters.MomentsAdapter
+import androidx.appcompat.app.AppCompatActivity
 import com.evgeny5454.exemplesfera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,26 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initRecyclerView()
-    }
-
-    private fun initRecyclerView(){
-        val addPhotos = binding.recyclerViewAddPhotos
-        val moments = binding.recyclerViewMoments
-        val chronicles = binding.recyclerViewChronicles
-
-        val addPhotosLayoutManager = LinearLayoutManager(this)
-        addPhotosLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
-        addPhotos.layoutManager = addPhotosLayoutManager
-        addPhotos.adapter = AddPhotoAdapter()
-
-        val momentsLayoutManager = LinearLayoutManager(this)
-        momentsLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
-        moments.layoutManager = momentsLayoutManager
-        moments.adapter = MomentsAdapter()
-
-        val chroniclesLayoutManager = GridLayoutManager(this, 3)
-        chronicles.layoutManager = chroniclesLayoutManager
-        chronicles.adapter = ChroniclesAdapter()
     }
 }
