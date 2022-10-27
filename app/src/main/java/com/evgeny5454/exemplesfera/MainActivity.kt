2dcc3b8f366c3bb469a8c164c4a8556238f4fea5
Dell <evgenyminaenkov@gmail.com>
2022-10-27 18:27:10 +0300

    Add Adapters

--- app/src/main/java/com/evgeny5454/exemplesfera/MainActivity.kt
+++ app/src/main/java/com/evgeny5454/exemplesfera/MainActivity.kt
@@ -11,7 +11,7 @@ import com.evgeny5454.exemplesfera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/evgeny5454/exemplesfera/adapters/AddPhotoAdapter.kt
@@ -0,0 +1,28 @@
+package com.evgeny5454.exemplesfera.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.evgeny5454.exemplesfera.databinding.ItemProfilePhotoBinding
+
+class AddPhotoAdapter : RecyclerView.Adapter<AddPhotoAdapter.PhotoViewHolder>() {
+
+    private val photosList = listOf(1, 2, 3, 4)
+
+    class PhotoViewHolder(binding: ItemProfilePhotoBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemProfilePhotoBinding.inflate(layoutInflater, parent, false)
+        return PhotoViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return photosList.size
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        //noting to do
+    }
+}
